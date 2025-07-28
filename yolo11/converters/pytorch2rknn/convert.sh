ef6567454aa6431a00581d2b7066950bf92aba31
Евгений Байдин <e.baydin@iva-cv.ru>
2025-07-28 22:44:13 +0300

    add pytorch to rknn

--- yolo11/converters/pytorch2rknn/convert.sh
+++ yolo11/converters/pytorch2rknn/convert.sh
@@ -25,23 +25,18 @@ for FILE in $MODEL_DIR/*.$MODEL_FORMAT; do
     MODEL_NAME_ONNX="${MODEL_NAME_PYTORCH%.*}.onnx"
     MODEL_NAME_RKNN="${MODEL_NAME_PYTORCH%.*}.rknn"
 
-    # Конвертируем .pt в onnx оптимизированный для rknn
+    # convert .pt to onnx optimised for rknn
     cp $FILE temp
     pushd "ultralytics_yolo11"
         export PYTHONPATH=./
-        sed -i "s|^model: .*|model: $pwd/temp/$MODEL_NAME_PYTORCH|" ./ultralytics/cfg/default.yaml  # Указываем путь к модели в конфиге ./ultralytics/cfg/default.yaml
+        sed -i "s|^model: .*|model: $pwd/temp/$MODEL_NAME_PYTORCH|" ./ultralytics/cfg/default.yaml
         python ./ultralytics/engine/exporter.py
     popd
 
-    # Конвертируем .pt в onnx оптимизированный для rknn
+    # convert .onnx to rknn
     pushd "rknn_model_zoo/examples/yolo11/python"
        python convert.py $pwd/temp/$MODEL_NAME_ONNX rk3588 fp $pwd/$MODEL_DIR/$MODEL_NAME_RKNN
     popd
-
-
-
-
-    #python convert.py --model_path $file
 done
 
 exit 0
